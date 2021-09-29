cda9a429f9b4cfc914ca877d02e3ccb7a4e1a63c
Nemeleks <mark62@yandex.ru>
2021-09-30 00:37:22 +0300

    1

--- Lesson1 4.27/Intermediate/Build/Win64/Lesson1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30133
-#include "C:/Users/Professional/Documents/Unreal Projects/UE4_GB/Lesson1 4.27/Intermediate/Build/Win64/Lesson1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
